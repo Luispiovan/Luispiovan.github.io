6e0a352b625bbda855aa11a05852834c675400d8
Luis Guilherme Piovan Costa <luis.costa@sawluz.com.br>
2021-02-18 09:31:44 -0300

    Updates

--- js/script.js
+++ js/script.js
@@ -23,20 +23,20 @@ $(document).ready(function() {
     $(".menu-btn i").toggleClass("active");
   });
 
-  // typing animation script
-  var typed = new Typed(".typing", {
-    strings: ["Student", "Developer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-  });
+  // // typing animation script
+  // var typed = new Typed(".typing", {
+  //   strings: ["Student", "Developer"],
+  //   typeSpeed: 100,
+  //   backSpeed: 60,
+  //   loop: true
+  // });
 
-  var typed = new Typed(".typing-2", {
-    strings: ["Student", "Developer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-  });
+  // var typed = new Typed(".typing-2", {
+  //   strings: ["Student", "Developer"],
+  //   typeSpeed: 100,
+  //   backSpeed: 60,
+  //   loop: true
+  // });
 
   // owl carousel script
   $(".carousel").owlCarousel({
